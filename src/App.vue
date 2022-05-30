55b077ea46adf4137b40f3b1e8fd3bf0427ece78
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2022-05-30 14:51:59 +0200

    add executeAttack, castSpell functions to Entity class

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,43 @@
 </template>
 
 <script>
+import { Heroes, Monsters } from './entities';
+
+class Entity {
+  constructor({ name, health, combatEfficiency, magicKnowledge }) {
+    (this.name = name),
+      (this.health = health),
+      (this.combatEfficiency = combatEfficiency),
+      (this.magicKnowledge = magicKnowledge);
+  }
+
+  executeAttack(enemy) {
+    enemy.takeDamage(this.combatEfficiency);
+  }
+
+  castSpell(enemy) {
+    enemy.takeDamage(this.magicKnowledge);
+  }
+
+  takeDamage(amount) {
+    this.health -= amount;
+    console.log(
+      `uderzono za ${amount} pkt. pozostało ${this.health} pkt. życia`
+    );
+  }
+}
+
+// for (const hero of Heroes) {
+//   Heroes[hero.name] = new Entity(hero);
+// }
+
+const heros = new Entity(Heroes[1]);
+const monster = new Entity(Monsters[0]);
+
+console.log(heros, monster);
+
+monster.castSpell(heros);
+
 export default {
   name: 'App',
 };
--- src/entities.js
+++ src/entities.js
@@ -2,5 +2,50 @@ export const Heroes = [
   {
     name: '_h1',
     health: 30,
+    combatEfficiency: 15,
+    magicKnowledge: 3,
+  },
+  {
+    name: '_h2',
+    health: 40,
+    combatEfficiency: 5,
+    magicKnowledge: 5,
+  },
+  {
+    name: '_h3',
+    health: 20,
+    combatEfficiency: 2,
+    magicKnowledge: 2,
+  },
+];
+
+export const Monsters = [
+  {
+    name: '_m1',
+    level: 1,
+    health: 15,
+    combatEfficiency: 10,
+    magicKnowledge: 2,
+  },
+  {
+    name: '_m2',
+    level: 1,
+    health: 18,
+    combatEfficiency: 4,
+    magicKnowledge: 4,
+  },
+  {
+    name: '_m3',
+    level: 1,
+    health: 10,
+    combatEfficiency: 7,
+    magicKnowledge: 1,
+  },
+  {
+    name: '_m4',
+    level: 1,
+    health: 30,
+    combatEfficiency: 3,
+    magicKnowledge: 2,
   },
 ];
